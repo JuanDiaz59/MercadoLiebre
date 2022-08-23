b78fafbb077625c7e1634f578dbf43f78be1a7e0
JuanDiaz59 <diazjuancruz59@gmail.com>
2022-08-23 20:24:38 -0300

    primer comit Mercado Liebre

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('Public'));
+
+
+
+app.listen(process.env.PORT || 5001, () => console.log('EL servidor esta corriendo en el puerto 5001'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './Views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './Views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './Views/login.html'))
+})
+
+app.post('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './Views/home.html'))
+})
